b3cef25ce6450e376c24c318a6b0e1bb945b6722
Alex Radcliffe <alexradcliffe@dalet.local>
2023-02-08 03:17:22 +0000

    please finally have worked

--- ope.py
+++ ope.py
@@ -4,6 +4,7 @@ import itertools
 import numpy as np
 from decimal import *
 import operator
+import sys
 
 
 class Polynomial:
@@ -145,14 +146,12 @@ def outputFromTxt(lmax, normalization, directory):
     y.insert(index, (1-sum(map(operator.mul, n, y))/n0))
     return y
 
-def OPE_bound(lmax, prec):
-    mac = True
+def OPE_bound(lmax, pythonPrec, sdpbPrec, procsPerNode, mac):
     if mac:
         directory = "/Users/alexradcliffe/kcl/hyperbolic-python"
     else:
         directory = "/users/k21187236/scratch/hyperbolic-python"
     os.chdir(directory)
-    getcontext().prec=prec
     nn = 6
     normalization = ["1"] + ["0"] * lmax
     objective = [str(F(nn, 2 * nn + l)(0)) for l in range(lmax + 1)]
@@ -180,17 +179,19 @@ def OPE_bound(lmax, prec):
     with open(f"{directory}/tmp/opepy{lmax}.json", "w") as f:
         json.dump(jsonInput, f, indent=4)
     if mac:
-        sdp2input = f"/usr/local/bin/docker run -v {directory}/tmp/:/usr/local/share/sdpb wlandry/sdpb:2.5.1 mpirun --allow-run-as-root -n 4 sdp2input --precision=1024 --input=/usr/local/share/sdpb/opepy{lmax}.json --output=/usr/local/share/sdpb/opepy{lmax}"
-        sdpb = f"/usr/local/bin/docker run -v {directory}/tmp/:/usr/local/share/sdpb wlandry/sdpb:2.5.1 mpirun --allow-run-as-root -n 4 sdpb --precision=1024 --procsPerNode=4 -s /usr/local/share/sdpb/opepy{lmax}"
+        sdp2input = f"/usr/local/bin/docker run -v {directory}/tmp/:/usr/local/share/sdpb wlandry/sdpb:2.5.1 mpirun --allow-run-as-root -n 4 sdp2input --precision={sdpbPrec} --input=/usr/local/share/sdpb/opepy{lmax}.json --output=/usr/local/share/sdpb/opepy{lmax}"
+        sdpb = f"/usr/local/bin/docker run -v {directory}/tmp/:/usr/local/share/sdpb wlandry/sdpb:2.5.1 mpirun --allow-run-as-root -n 4 sdpb --precision={sdpbPrec} --procsPerNode={procsPerNode} -s /usr/local/share/sdpb/opepy{lmax}"
     else:
-        sdp2input = f"sdp2input --precision=1024 --input={directory}/tmp/opepy{lmax}.json --output={directory}/tmp/opepy{lmax}"
-        sdpb = f"sdpb --precision=1024 --procsPerNode=4 -s {directory}/tmp/opepy{lmax}"
+        sdp2input = f"sdp2input --precision={sdpbPrec} --input={directory}/tmp/opepy{lmax}.json --output={directory}/tmp/opepy{lmax}"
+        sdpb = f"sdpb --precision={sdpbPrec} --procsPerNode={procsPerNode} -s {directory}/tmp/opepy{lmax}"
     os.system(sdp2input)
     os.system(sdpb)
     bound = read_output(lmax, directory)
     coefficients = outputFromTxt(lmax, normalization, directory)
     jsonResults = {"lmax" : lmax,
-                "prec" : prec,
+                "pythonPrec" : pythonPrec,
+                "sdpbPrec" : sdpbPrec,
+                "procsPerNode" : procsPerNode,
                 "bound" : str(bound),
                 "coefficients" : list(map(str, coefficients))}
     with open(f"{directory}/outputJsons/opepy{lmax}_out.json", "w") as f:
@@ -200,4 +201,17 @@ def OPE_bound(lmax, prec):
 #     print(f"lmax={i}")
 #     OPE_bound(i, 200)
 
-OPE_bound(7, 200)
+
+if __name__ == "__main__":
+    config_file = sys.argv[1]
+    with open(config_file) as f:
+        config = json.load(f)
+    pythonPrec = config["pythonPrec"]
+    sdpbPrec = config["sdpbPrec"]
+    lmaxLower = config["lmaxLower"]
+    lmaxUpper = config["lmaxUpper"]
+    procsPerNode = config["procsPerNode"]
+    mac = config["mac"]
+    getcontext().prec = pythonPrec
+    for lmax in range(lmaxLower, lmaxUpper + 1):
+        OPE_bound(lmax, pythonPrec, sdpbPrec, procsPerNode, mac)
